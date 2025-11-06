254f9b1a1280e735d7f95a75e16d352440db1680
Marie Peron <marie.prn77@gmail.com>
2025-11-06 15:40:07 +0100

    FonctionnementModale

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,25 +3,43 @@ const emailCorrect = "sophie.bluel@test.tld";
 const mdpCorrect = "S0phie";
 const form = document.querySelector(".formLogIn");
 
-if(form){
-form.addEventListener("submit",(e) => {
+if (form) {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const email = form.elements["username"].value;
-    const mdp = form.elements["password"].value;
+    const password = form.elements["password"].value;
 
+    // On supprime un éventuel ancien message d'erreur
     const ancienMsg = form.querySelector(".erreur");
     if (ancienMsg) ancienMsg.remove();
 
-    if(email === emailCorrect && mdp === mdpCorrect){
+    try {
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Connexion réussie enregistre le token
+        localStorage.setItem("token", data.token);
         localStorage.setItem("connecte", "true");
+
         window.location.href = "index.html";
-    }
-    else{
+      } else {
         const msgErreur = document.createElement("p");
-        msgErreur.textContent = "Erreur dans l'identifiant ou le mot de passe"
+        msgErreur.textContent = "Erreur dans l'identifiant ou le mot de passe";
+        msgErreur.classList.add("erreur");
         form.appendChild(msgErreur);
-        msgErreur.classList.add("erreur")
+      }
+    } catch (error) {
+      console.error("Erreur de connexion à l'API :", error);
+      const msgErreur = document.createElement("p");
+      msgErreur.textContent = "Impossible de se connecter au serveur.";
+      form.appendChild(msgErreur);
     }
-});
+  });
 }
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -148,16 +148,243 @@ function activeModeEdition() {
     //Modale
     const modale = document.querySelector(".arrierePlanModale");
     const iconeFermer = document.querySelector(".iconeFermer");
+    const vueGalerie = document.querySelector(".modaleGalerie");
+    const vueFormulaire = document.querySelector(".formulaire");
+    const modificationGalerie = document.querySelector(".modificationgalerie");
+    const btnAjouterPhoto = document.querySelector(".bntAjoutPhoto");
+    const btnRetour = document.querySelector(".formulaire img");
+    const formAjout = document.getElementById("formAjout");
+    const selectCategorie = formAjout.querySelector("select[name='categoryId']");
 
+    //ouvrir
     boutonModifier.addEventListener("click", () => {
         modale.style.display = "flex";
+        vueGalerie.style.display = "block";
+        vueFormulaire.style.display = "none";
+        remplirGalerieModale();
     });
 
-    iconeFermer.addEventListener("click", () => {
+    //fermer
+    iconeFermer.addEventListener("click", () => modale.style.display = "none");
+    modale.addEventListener("click", e => { if(e.target === modale) modale.style.display = "none"; });
+
+    //allez au formulaire 
+      btnAjouterPhoto.addEventListener("click", () => {
+        vueGalerie.style.display = "none";
+        vueFormulaire.style.display = "block";
+    });
+
+    //sortir du formulaire 
+     btnRetour.addEventListener("click", () => {
+        vueFormulaire.style.display = "none";
+        vueGalerie.style.display = "block";
+    });
+
+    //affichez projet dans la modale
+     function remplirGalerieModale() {
+        modificationGalerie.innerHTML = "";
+        allWorks.forEach(work => {
+            const figure = document.createElement("figure");
+
+            const img = document.createElement("img");
+            img.src = work.imageUrl;
+            img.alt = work.title;
+
+            const btnSupprimer = document.createElement("img");
+            btnSupprimer.src = "assets/icons/poubelle.svg";
+            btnSupprimer.classList.add("poubelle")
+            btnSupprimer.addEventListener("click", async () => {
+                const confirmation = confirm("Vous êtes sûr de vouloir supprimer ce projet ?");
+                
+                if (!confirmation) return;
+                const token = localStorage.getItem("token");
+                if (!token) {
+                    alert("Vous devez être connecté pour supprimer un projet !");
+                    return;
+                }
+
+                try {
+                    const response = await fetch(`http://localhost:5678/api/works/${work.id}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                    });
+
+                    if (response.ok) {
+                    //Supprimer dans la modale
+                    figure.remove();
+
+                    //Supprimer dans le tableau
+                    allWorks = allWorks.filter(w => w.id !== work.id);
+
+                    //Supprimer dans la galerie principale
+                    const galleryFigures = document.querySelectorAll(".gallery figure");
+                    galleryFigures.forEach(fig => {
+                        const img = fig.querySelector("img");
+                        if (img && img.src === work.imageUrl) {
+                        fig.remove();
+                        }
+                    });
+
+                    } else {
+                    console.error("Erreur lors de la suppression :", response.status);
+                    alert("La suppression a échoué.");
+                    }
+                } catch (error) {
+                    console.error("Erreur réseau :", error);
+                    alert("Impossible de contacter le serveur.");
+                }
+                });
+
+            figure.appendChild(img);
+            figure.appendChild(btnSupprimer);
+            modificationGalerie.appendChild(figure);
+        });
+     }
+     async function chargerCategories() {
+        const response = await fetch("http://localhost:5678/api/categories");
+        const categories = await response.json();
+        selectCategorie.innerHTML = "";
+        categories.forEach(cat => {
+            const option = document.createElement("option");
+            option.value = cat.id;
+            option.textContent = cat.name;
+            selectCategorie.appendChild(option);
+        });
+    }
+    chargerCategories();
+
+    // Soumission du formulaire
+    formAjout.addEventListener("submit", e => {
+        e.preventDefault();
+        const nouveauTravail = {
+            title: formAjout.title.value,
+            imageUrl: formAjout.imageUrl.value,
+            categoryId: parseInt(formAjout.categoryId.value),
+        };
+        allWorks.push(nouveauTravail);
+        remplirGalerieModale();
+        vueFormulaire.style.display = "none";
+        vueGalerie.style.display = "block";
+        formAjout.reset();
+    });
+
+    //ajout projet
+    const formAdd = document.querySelector("#formAjout");
+    const inputImage = document.querySelector("#image");
+    const previewImage = document.querySelector("#preview");
+    const inputTitle = formAdd.querySelector("input[name='title']");
+    const selectCategory = formAdd.querySelector("select[name='categoryId']");
+    const btnValider = document.querySelector("#btnValider");
+    const zoneUpload = document.querySelector(".zoneUpload");
+
+    //visualisation img
+    inputImage.addEventListener("change", () => {
+    const file = inputImage.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = e => {
+        previewImage.src = e.target.result;
+        previewImage.style.display = "block";
+        btnValider.disabled = false;
+        zoneUpload.classList.add("has-preview");
+    };
+    reader.readAsDataURL(file);
+    });
+
+    //appel catégorie
+    async function chargerCategories() {
+    try {
+        const response = await fetch("http://localhost:5678/api/categories");
+        const categories = await response.json();
+
+        selectCategory.innerHTML = "";
+        categories.forEach(cat => {
+        const option = document.createElement("option");
+        option.value = cat.id;
+        option.textContent = cat.name;
+        selectCategory.appendChild(option);
+        });
+    } catch (error) {
+        console.error("Erreur lors du chargement des catégories :", error);
+    }
+    }
+    chargerCategories();
+
+    //maj galerie
+    function afficherGaleriePrincipale() {
+    gallery.innerHTML = "";
+    allWorks.forEach(work => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+    }
+
+    //formualaire
+    formAdd.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+        alert("Vous devez être connecté pour ajouter un projet !");
+        return;
+    }
+
+    const image = inputImage.files[0];
+    const title = inputTitle.value.trim();
+    const category = selectCategory.value;
+
+    if (!image || !title || !category) {
+        alert("Merci de remplir tous les champs !");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", category);
+
+    try {
+        const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        body: formData
+        });
+
+        if (response.ok) {
+        const newWork = await response.json();
+
+        allWorks.push(newWork);
+        afficherGaleriePrincipale();
+        remplirGalerieModale();
+
+        //remettre à 0 le formulaire
+        formAdd.reset();
+        previewImage.style.display = "none";
+        btnValider.disabled = true;
+        zoneUpload.classList.remove("has-preview");
         modale.style.display = "none";
-    });
 
-    modale.addEventListener("click", (e) => {
-        if (e.target === modale) modale.style.display = "none";
+        } else {
+        alert("Erreur lors de l'ajout du projet !");
+        }
+    } catch (error) {
+        console.error("Erreur réseau :", error);
+        alert("Impossible de contacter le serveur.");
+    }
     });
+    
 }
